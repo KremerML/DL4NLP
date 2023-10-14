50e225ab3fd77cb5adb863cee4e6d210590b9536
uvandre <57217247+uvandre@users.noreply.github.com>
2023-10-14 17:11:54 +0200

    add bert finetuning notebook and data

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/jzbjyb/X-FACTR.git
+# git clone https://github.com/jzbjyb/X-FACTR.git
 
 # before install, make sure you have git-lfs (otherwise X-FACTR will not install correctly)
 apt-get install git-lfs
 
+# first conda environment for baseline reproduction
 conda create -n xfactr -y python=3.7 && conda activate xfactr && X-FACTR/setup.sh
 
 pip install -r X-FACTR/requirements.txt
@@ -15,3 +16,4 @@ pip install protobuf==3.20.*
 # the version of torch provided in requirements.txt is outdated and does not work 
 # 1.13.1+cu117
 pip install --upgrade torch
+
